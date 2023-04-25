2bc03838329ec76a2b1d80f5bdb92d55a450a3aa
Jp8125 <190170116055@vgec1.gujgov.edu.in>
2023-04-25 10:37:45 +0530

    routing log out added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  constructor(private authService: AuthService, private router: Router){}
+  title = 'inv_sys';
+  log_out(){
+    this.authService.logout()
+    this.router.navigate(['/sign-in']);
 
+  }
 }
